a7f92d52df8f0ef1be86429f82d855afab393318
Ben Ruwel <ruwelmwangi@gmail.com>
2022-12-09 11:35:24 +0300

    Complete chat functionality using socket io rooms

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
-const app = express();
 const http = require("http");
+
+const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -18,7 +18,7 @@ const io = require("socket.io")(server, {
 // CORS setup
 let whiteListedUrls = ["http://localhost:4200/"];
 
-var corsOptions = {
+let corsOptions = {
   origin: function (origin, callback) {
     if (whiteListedUrls.indexOf(origin) !== -1) {
       callback(null, true);
@@ -28,68 +28,45 @@ var corsOptions = {
   },
 };
 
-const emitNewMessageEvent = (msg) => {
-  const timestamp = new Date().getTime();
-  const messageId = uuidv4();
-  const messageObj = {
-    timestamp: timestamp.toString(),
-    messageId: messageId,
-    message: msg,
-  };
-  console.log(messageObj);
-  io.emit("new-message", messageObj);
-  return messageObj;
-};
-
 /* MIDDLEWARE **/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static("pages"));
 
 // all origins
 app.use(cors());
 
-// app.use(helmet());
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/pages/index.html");
+  res.sendFile(__dirname + "/pages/home.html");
 });
 
-app.get("/api/hello", (req, res) => {
-  res.send({
-    message: "Yep api works",
-  });
+app.get("/room/:roomId", (req, res) => {
+  res.sendFile(__dirname + "/pages/room.html");
 });
 
 app.post("/api/new-message", (req, res) => {
-  const message = req.body?.message;
-  const messageEmitted = emitNewMessageEvent(message);
+  const payload = req.body;
+  const messageId = uuidv4();
+  const messageObj = {
+    messageId,
+    ...payload,
+  };
+  io.to(messageObj.roomId).emit("new-message", messageObj);
   res.send({
     status: "successful",
-    data: messageEmitted,
+    data: messageObj,
   });
 });
 
 // listening to events from client
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("new-message", (msg) => {
-    // sends this message event to everyone including the sender
-    io.emit("new-message", msg);
-    console.log(msg);
+  socket.on("join-room", (payload) => {
+
+    console.log('New room created', payload.roomId)
+    socket.join(payload.roomId);
   });
 });
 
-// emit events from server to everyone
-// io.emit("some event", {
-//   someProp: "some value",
-//   otherProp: "other value",
-// });
-
-// emit event for everyone except the sender
-// io.on("connection", (socket) => {
-//   socket.broadcast.emit('hi')
-// });
-
-server.listen(3000, () => {
-  console.log("Listening on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port ::: ", process.env.PORT || 3000);
 });
